864eacca1f754449abcd09ef777f2666d632927c
Hugo Astudilo <hugoast90@gmail.com>
2024-09-25 16:45:44 -0300

    User and token test, views and url

---
+++ app/user/views.py
@@ -0,0 +1,22 @@
+"""
+Views for the user API.
+"""
+
+from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
+from user.serializers import AuthTokenSerializer, UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    """Create a new user in the system"""
+
+    serializer_class = UserSerializer
+
+
+class CreateTokeView(ObtainAuthToken):
+    """Create a new auth token for use"""
+
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_REDERER_CLASSES
